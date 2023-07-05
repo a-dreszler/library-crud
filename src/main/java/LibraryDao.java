2aa062e55ee5ee87b555682c7e222cee5c3c4998
Aleksander Dreszler <olek.d93@gmail.com>
2023-07-05 16:34:02 +0200

    Created DAO class

---
+++ src/main/java/LibraryDao.java
@@ -0,0 +1,105 @@
+import java.sql.*;
+import java.util.Optional;
+
+class LibraryDao {
+
+    Connection connection;
+
+    public LibraryDao() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "admin");
+        } catch (SQLException e) {
+            throw new RuntimeException("Nie udało się połączyć z bazą danych:\n" + e.getMessage());
+        }
+    }
+
+    boolean librarySave(Book book) {
+        final String sql = """
+                INSERT INTO book (isbn, title, author, year)
+                VALUES (?, ?, ?, ?)
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, book.getIsbn());
+            statement.setString(2, book.getTitle());
+            statement.setString(3, book.getAuthor());
+            statement.setInt(4, book.getYear());
+            int rowsUpdated = statement.executeUpdate();
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            System.out.println("Nie udało się zapisać książki:\n" + e.getMessage());
+        }
+
+        return false;
+    }
+
+    Optional<Book> libraryRead(String isbn) {
+        final String sql = """
+                SELECT
+                    *
+                FROM
+                    book
+                WHERE
+                    isbn = ?
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, isbn);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return Optional.of(new Book(
+                        resultSet.getString("title"),
+                        resultSet.getString("author"),
+                        resultSet.getInt("year"),
+                        resultSet.getString("isbn")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println("Nie udało się odczytać książki:\n" + e.getMessage());
+        }
+
+        return Optional.empty();
+    }
+
+    int libraryUpdate(Book book) {
+        final String sql = """
+                UPDATE book
+                SET
+                    isbn = ?,
+                    title = ?,
+                    author = ?,
+                    year = ?
+                WHERE
+                    isbn = ?;
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, book.getIsbn());
+            statement.setString(2, book.getTitle());
+            statement.setString(3, book.getAuthor());
+            statement.setInt(4, book.getYear());
+            statement.setString(5, book.getIsbn());
+            int rowsUpdated = statement.executeUpdate();
+
+            return rowsUpdated;
+        } catch (SQLException e) {
+            System.out.println("Nie udało się zaktualizować książki:\n" + e.getMessage());
+        }
+
+        return 0;
+    }
+
+    int libraryDelete(String isbn) {
+        final String sql = """
+                DELETE FROM book WHERE isbn = ?
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, isbn);
+            int rowsDeleted = statement.executeUpdate();
+
+            return rowsDeleted;
+        } catch (SQLException e) {
+            System.out.println("Nie udało się usunąć książki:\n" + e.getMessage());
+        }
+
+        return 0;
+    }
+}
